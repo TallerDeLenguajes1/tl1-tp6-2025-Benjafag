f7623758c18dcff64fc7a1c528675ef26d3decda
Benjafag <benja_fag_04@hotmail.com>
2025-05-20 15:13:12 -0300

    ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// 1
+int numero;
+Console.WriteLine("Numero: ");
+
+string cadena = Console.ReadLine();
+bool convertido = int.TryParse(cadena, out numero);
+
+Console.WriteLine((convertido && numero > 0) ? invertirNum(numero) : "Numero invalido");
+
+int invertirNum(int a)
+{
+  int result = 0;
+  while (a >= 10)
+  {
+    result += a % 10;
+    result *= 10;
+    a /= 10;
+  }
+  return result + a % 10;
+}
+
+
+// 2 
