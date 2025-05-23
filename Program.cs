c29e2e5739f20229aa93c92b5029f9018f87a536
Benjafag <benja_fag_04@hotmail.com>
2025-05-22 22:49:04 -0300

    ejercicio 4

--- Program.cs
+++ Program.cs
@@ -1,100 +1,164 @@
-﻿// // 1
-// using System.Diagnostics.Contracts;
+﻿// 1
 
-// int numero;
-// Console.WriteLine("Numero: ");
+int numero;
+Console.WriteLine("Numero: ");
 
-// string buff = Console.ReadLine();
-// bool convertido = int.TryParse(buff, out numero);
+string buff = Console.ReadLine();
+bool convertido = int.TryParse(buff, out numero);
 
-// Console.WriteLine((convertido && numero > 0) ? invertirNum(numero) : "Numero invalido");
+Console.WriteLine((convertido && numero > 0) ? invertirNum(numero) : "Numero invalido");
 
-// int invertirNum(int a)
-// {
-//   int result = 0;
-//   while (a >= 10)
-//   {
-//     result += a % 10;
-//     result *= 10;
-//     a /= 10;
-//   }
-//   return result + a % 10;
-// }
-
-
-// // 2 y 3
-// int seguir;
-// int seleccion;
-// double primNumero = -9999, segNumero = -9999;
-// double result;
-// bool convers1 = true, convers2 = true;
-// do
-// {
-//   do
-//   {
-//     Console.WriteLine("CALCULADORA\n[1]Sumar\n[2]Restar\n[3]Multiplicar\n[4]Dividir\n[5]Valor absoluto\n[6]Cuadrado\n[7]Raiz\n[8]Seno\n[9]Coseno\n[10]Parte entera");
-//     buff = Console.ReadLine();
-//     int.TryParse(buff, out seleccion);
-//   } while (1 > seleccion || seleccion > 10);
-//   Console.WriteLine(seleccion > 4 ? "Numero: " : "Primer numero: ");
-//   buff = Console.ReadLine();
-//   convers1 = double.TryParse(buff, out primNumero);
-//   if (seleccion <= 4)
-//   {
-//     Console.WriteLine("Segundo numero: ");
-//     buff = Console.ReadLine();
-//     convers2 = double.TryParse(buff, out segNumero);
-//   }
-//   if (convers1 && convers2)
-//   {
-//     switch (seleccion)
-//     {
-//       case 1: result = primNumero + segNumero; break;
-//       case 2: result = primNumero - segNumero; break;
-//       case 3: result = primNumero * segNumero; break;
-//       case 4: result = primNumero / segNumero; break;
-//       case 5: result = Math.Abs(primNumero); break;
-//       case 6: result = Math.Pow(primNumero, 2); break;
-//       case 7: result = Math.Sqrt(primNumero); break;
-//       case 8: result = Math.Sin(primNumero); break;
-//       case 9: result = Math.Cos(primNumero); break;
-//       case 10: result = Math.Truncate(primNumero); break;
-//       default: result = -9999; break;
-//     }
-//     Console.WriteLine("Resultado: " + result);
-//   }
-//   else
-//   {
-//     Console.WriteLine("Numero/s no valido/s");
-//   }
-//   Console.WriteLine("Desea realizar otra operacion: \n[1] Si\n[0] No");
-//   buff = Console.ReadLine();
-//   int.TryParse(buff, out seguir);
-// } while (seguir != 0);
+int invertirNum(int a)
+{
+  int result = 0;
+  while (a >= 10)
+  {
+    result += a % 10;
+    result *= 10;
+    a /= 10;
+  }
+  return result + a % 10;
+}
 
 
-//4
-// int numero = 54;
-// string cadena = @$"Ho\n{numero}la";
-// Console.WriteLine(cadena);
+// 2 y 3
+int seguir;
+int seleccion;
+double primNumero = -9999, segNumero = -9999;
+double result;
+bool convers1 = true, convers2 = true;
+do
+{
+  do
+  {
+    Console.WriteLine("CALCULADORA\n[1]Sumar\n[2]Restar\n[3]Multiplicar\n[4]Dividir\n[5]Valor absoluto\n[6]Cuadrado\n[7]Raiz\n[8]Seno\n[9]Coseno\n[10]Parte entera");
+    buff = Console.ReadLine();
+    int.TryParse(buff, out seleccion);
+  } while (1 > seleccion || seleccion > 10);
+  Console.WriteLine(seleccion > 4 ? "Numero: " : "Primer numero: ");
+  buff = Console.ReadLine();
+  convers1 = double.TryParse(buff, out primNumero);
+  if (seleccion <= 4)
+  {
+    Console.WriteLine("Segundo numero: ");
+    buff = Console.ReadLine();
+    convers2 = double.TryParse(buff, out segNumero);
+  }
+  if (convers1 && convers2)
+  {
+    switch (seleccion)
+    {
+      case 1: result = primNumero + segNumero; break;
+      case 2: result = primNumero - segNumero; break;
+      case 3: result = primNumero * segNumero; break;
+      case 4: result = primNumero / segNumero; break;
+      case 5: result = Math.Abs(primNumero); break;
+      case 6: result = Math.Pow(primNumero, 2); break;
+      case 7: result = Math.Sqrt(primNumero); break;
+      case 8: result = Math.Sin(primNumero); break;
+      case 9: result = Math.Cos(primNumero); break;
+      case 10: result = Math.Truncate(primNumero); break;
+      default: result = -9999; break;
+    }
+    Console.WriteLine("Resultado: " + result);
+  }
+  else
+  {
+    Console.WriteLine("Numero/s no valido/s");
+  }
+  Console.WriteLine("Desea realizar otra operacion: \n[1] Si\n[0] No");
+  buff = Console.ReadLine();
+  int.TryParse(buff, out seguir);
+} while (seguir != 0);
+
+
+// 4
 Console.WriteLine("Ingrese un texto: ");
 string cadena = Console.ReadLine();
-Console.WriteLine($"Longitud: {cadena.Length}");
-// Console.WriteLine("Ingrese otro texto: ");
-// string cadena2 = Console.ReadLine();
-// Console.WriteLine($"Concatenacion: {cadena + cadena2}");
-Console.WriteLine($"Subcadena: {cadena.Substring(cadena.Length / 2)}");
-// foreach (char caracter in cadena)
-// {
-//   Console.Write(caracter);
-// }
 
-// float[] numeros = { 10.15f, 25, 38.1f, 200 };
-// foreach (float numero in numeros)
-// {
-//   Console.WriteLine(numero.ToString("N"));
-// }
-Console.WriteLine("Palabra a buscar");
+// Obtener la longitud de la cadena y muestre por pantalla.
+int longitud = cadena.Length;
+Console.WriteLine($"Longitud: {longitud}");
+
+// A partir de una segunda cadena ingresada por el usuario, concatene ambas cadenas.
+Console.WriteLine("Ingrese otro texto: ");
+string cadena2 = Console.ReadLine();
+string concatenacion = cadena + cadena2;
+Console.WriteLine($"Concatenacion: {concatenacion}");
+
+// Extraer una subcadena de la cadena ingresada.
+string subcadena = cadena.Substring(longitud / 2);
+Console.WriteLine($"Subcadena: {subcadena}");
+
+// Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por elemento en pantalla.
+foreach (var caracter in cadena)
+{
+  Console.WriteLine(caracter);
+}
+
+// Buscar la ocurrencia de una palabra determinada en la cadena ingresada.
+Console.WriteLine($"Palabra a buscar en {cadena}: ");
 string buscada = Console.ReadLine();
-int indice = cadena.IndexOf(buscada);
-Console.WriteLine($"La subcadena " + (indice != -1 ? "SI" : "NO") + "esta en la cadena");
+int coincidencias = 0, indice = cadena.IndexOf(buscada);
+while (indice != -1)
+{
+  coincidencias++;
+  indice = cadena.IndexOf(buscada, indice + 1);
+}
+Console.WriteLine($"Se encontró \"{buscada}\" {coincidencias} veces en \"{cadena}\"");
+
+// Convierta la cadena a mayúsculas y luego a minúsculas.
+string mayusculas = cadena.ToUpper();
+string minusculas = cadena.ToLower();
+Console.WriteLine($"Cadena en mayusculas: {mayusculas} \nCadena en minusculas: {minusculas}");
+
+// Ingrese una cadena separada por caracteres que usted determine y muestre por pantalla los resultados (Revisar el comportamiento de split())
+string[] separado = cadena.Split();
+foreach (var palabra in separado)
+{
+  Console.WriteLine(palabra);
+}
+
+// Ingrese una ecuación simple como cadena de caracteres y que el sistema lo resuelva. (582+2 => 584)
+Console.WriteLine("Ingrese una ecuacion simple (a op b)");
+string ecuacion = Console.ReadLine();
+int indiceOp = indiceOperador(ecuacion);
+if (indiceOp != -1)
+{
+  char operador = ecuacion[indiceOp];
+  string[] nums = ecuacion.Split(operador);
+  int a, b;
+  bool convA, convB;
+  convA = int.TryParse(nums[0], out a);
+  convB = int.TryParse(nums[1], out b);
+  if (convA && convB)
+  {
+    double resultado = 0;
+    switch (operador)
+    {
+      case '+': resultado = a + b; break;
+      case '-': resultado = a - b; break;
+      case '/': resultado = a / b; break;
+      case '*': resultado = a * b; break;
+
+    }
+    Console.WriteLine($"Resultado = {resultado}");
+  }
+  else
+  {
+    Console.WriteLine("Expresion no formulada correctamente");
+  }
+}
+
+int indiceOperador(string cadena)
+{
+  string operadores = "+-/*";
+  int resultado = -1;
+  foreach (var operador in operadores)
+  {
+    resultado = cadena.IndexOf(operador);
+    if (resultado != -1)
+      break;
+  }
+  return resultado;
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/24pP-Pw_)
+# ¿String es una tipo por valor o un tipo por referencia?
+
+- String es uno de los tipos de referencia integrados de C#.
+
+# ¿Qué secuencias de escape tiene el tipo string?
+
+- En C#, las secuencias de escape comienzan con una barra invertida ('\') seguida de un caracter o codigo Unicode que representa el caracter especial deseado, como por ejemplo: '\n', '\t', '\0', etc.
+
+# ¿Qué sucede cuando utiliza el caracter @ y $ antes de una cadena de texto?
+
+- Agregar el caracter @ previo a un string nos lo pasará a texto plano, es decir ignorará las secuencias de escape que contenga, mientras que el $ nos permitirá insertar valores de variables de una manera sencilla en un string (interpolacion) para una mejor lectura del codigo.
+
+# Expresiones Regulares
+
+- Las expresiones regulares (también conocidas como regex) son patrones utilizados para buscar, coincidir o manipular texto. Se componen de una secuencia de caracteres que definen un conjunto de cadenas posibles. Son una herramienta muy poderosa para trabajar con cadenas, ya que permiten realizar búsquedas y validaciones complejas con muy pocas líneas de código.
+
+## ¿Funcionan únicamente en C#?
+
+- No, las expresiones regulares no son exclusivas de C#. Se utilizan en muchos lenguajes de programación. Lo que puede variar ligeramente entre lenguajes es la sintaxis o el motor de expresiones regulares que se utilice, pero en general los conceptos son los mismos.
+
+## ¿En qué casos le parecen útiles? Enuncie al menos 3.
+
+- Validar entradas del usuario
+- Buscar patrones en textos
+- Reemplazar texto automáticamente
+
+# ¿Cómo se hace uso de estas en C#?
+
+- En C#, se usan a través de la clase System.Text.RegularExpressions.Regex
